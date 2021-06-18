5b5dfc10f3186eb7c123aa04f4014f39bcdedbaf
victoriatarane <victoriatarane@gmail.com>
2021-06-17 23:58:14 -0700

    phase 5

--- backend/routes/api/session.js
+++ backend/routes/api/session.js
@@ -6,6 +6,21 @@ const { User } = require('../../db/models');
 
 const router = express.Router();
 
+const { check } = require('express-validator');
+const { handleValidationErrors } = require('../../utils/validation');
+
+const validateLogin = [
+    check('credential')
+        .exists({ checkFalsy: true })
+        .notEmpty()
+        .withMessage('Please provide a valid email or username.'),
+    check('password')
+        .exists({ checkFalsy: true })
+        .withMessage('Please provide a password.'),
+    handleValidationErrors,
+];
+
+
 router.post(
     '/',
     asyncHandler(async (req, res, next) => {
@@ -50,4 +65,30 @@ router.get(
     }
 );
 
+router.post(
+    '/',
+    validateLogin,
+    asyncHandler(async (req, res, next) => {
+        const { credential, password } = req.body;
+
+        const user = await User.login({ credential, password });
+
+        if (!user) {
+            const err = new Error('Login failed');
+            err.status = 401;
+            err.title = 'Login failed';
+            err.errors = ['The provided credentials were invalid.'];
+            return next(err);
+        }
+
+        await setTokenCookie(res, user);
+
+        return res.json({
+            user,
+        });
+    }),
+);
+
+
 module.exports = router;
+
