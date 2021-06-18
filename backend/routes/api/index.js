c7ceb068876f3b3de4cce730b4a5e06c6d151a38
victoriatarane <victoriatarane@gmail.com>
2021-06-17 19:16:18 -0700

    phase 1

---
+++ backend/routes/api/index.js
@@ -0,0 +1,19 @@
+const router = require('express').Router();
+
+// router.post('/test', function (req, res) {
+//     res.json({ requestBody: req.body });
+// });
+
+
+module.exports = router;
+
+
+//In console 
+// fetch('/api/test', {
+//     method: "POST",
+//     headers: {
+//         "Content-Type": "application/json",
+//         "XSRF-TOKEN": `Noa2bMhJ-MlNOhDY0M9ZfVw8VROf_r9FDbLE` //the current value
+//     },
+//     body: JSON.stringify({ hello: 'world' })
+// }).then(res => res.json()).then(data => console.log(data));
